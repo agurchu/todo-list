6ef5d3b21e8b9081498ca8ad941db77e2bb990fb
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-03-31 20:40:58 +0200

    Links to Header

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 function Header() {
   /* ======================= Return ========================*/
   return (
     <HeaderTodo>
       <h1>Todo List</h1>
+      <ul>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+      </ul>
     </HeaderTodo>
   );
 }
@@ -19,9 +24,24 @@ export default Header;
 const HeaderTodo = styled.header`
   background: #1a2b26;
   color: white;
+  display: flex;
   width: 100%;
-  text-align: center;
-  padding: 10px;
+  align-items: center;
+  justify-content: space-between;
+  padding: 10px 30px;
   margin-bottom: 40px;
   box-shadow: 0 4px 7px 2px rgba(0, 0, 0, 0.3);
+
+  h1 {
+    font-size: 16px;
+  }
+  ul a {
+    color: white;
+    margin-left: 20px;
+    transition: 250ms;
+
+    &:hover {
+      color: #2c9476;
+    }
+  }
 `;
